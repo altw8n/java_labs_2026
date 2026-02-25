92ef2182b3a63f7ccb673fa694c2eaad73526b1a
altw8n <altwn8@gmail.com>
2026-02-25 12:51:10 +0300

    task 15

--- src/main/java/org/example/chapter01/task15/Main.java
+++ src/main/java/org/example/chapter01/task15/Main.java
@@ -4,4 +4,29 @@
 package org.example.chapter01.task15;
 
 public class Main {
+    static void main() {
+        int month = 4;
+        switch (month){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("outemn");
+                break;
+        }
+    }
 }
