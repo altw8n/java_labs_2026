2a538cd737b1e6921eb6391d8eff97aa94977b25
altw8n <altwn8@gmail.com>
2026-03-05 09:39:23 +0300

    tasks 6 and 7

---
+++ src/main/java/org/example/chapter02/task07/Main.java
@@ -0,0 +1,55 @@
+/*В большинстве IDE есть средства для быстрого написания такого шаблонного кода.
+Что предлагает ваша IDE?*/
+
+package org.example.chapter02.task07;
+
+import java.util.Objects;
+
+class Point{
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(x, point.x) == 0 && Double.compare(y, point.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
+public class Main {
+}
