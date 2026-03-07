d4b1fac4d0f307cd040950fa3cad62c3f4d960f0
altw8n <altwn8@gmail.com>
2026-03-07 12:24:19 +0300

    WIP: task 09

--- src/main/java/org/example/chapter02/task09/Main.java
+++ src/main/java/org/example/chapter02/task09/Main.java
@@ -1,4 +1,23 @@
 package org.example.chapter02.task09;
 
+record TimeOfDay(int hour, int minute){
+    public TimeOfDay{
+        if (minute < 0 || minute >= 60){
+            int extraH = minute / 60;
+            minute = minute % 60;
+            if (minute < 0){
+                minute += 60;
+                extraH--;
+            }
+            hour += extraH;
+        }
+        if (hour < 0){
+            hour = 24 - (Math.abs(hour) % 24);
+        } else {
+            hour = hour % 24;
+        }
+    }
+}
+
 public class Main {
 }
