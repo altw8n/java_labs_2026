da0ab5f9844bdadec789e5d33d6092a63b6a8e04
altw8n <altwn8@gmail.com>
2026-04-09 20:02:17 +0300

    task 15

---
+++ src/main/java/org/example/chapter03/task15/Employee.java
@@ -0,0 +1,24 @@
+package org.example.chapter03.task15;
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %.2f", name, salary);
+    }
+}
