e15163b4b87b4ee2706d74e09b53e74b97c01afc
altw8n <altwn8@gmail.com>
2026-04-10 00:19:18 +0300

    task 16

---
+++ src/main/java/org/example/chapter03/task16/Sequences.java
@@ -0,0 +1,46 @@
+package org.example.chapter03.task16;
+
+import java.util.random.RandomGenerator;
+
+public class Sequences {
+    private static final RandomGenerator GENERATOR = RandomGenerator.getDefault();
+
+    public static class RandomSequence implements IntSequence {
+        private final int low;
+        private final int high;
+        private final int range;
+
+        public RandomSequence(int low, int high) {
+            if (low > high) {
+                throw new IllegalArgumentException(
+                        "low (" + low + ") must be <= high (" + high + ")"
+                );
+            }
+            this.low = low;
+            this.high = high;
+            this.range = high - low + 1;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public int next() {
+            return low + GENERATOR.nextInt(range);
+        }
+
+        public int getLow() {
+            return low;
+        }
+
+        public int getHigh() {
+            return high;
+        }
+    }
+
+    public static IntSequence randomInts(int low, int high) {
+        return new RandomSequence(low, high);
+    }
+}
